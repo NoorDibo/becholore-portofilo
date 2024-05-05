7e83cad90f67776b22a5aa8692348f765ef2ed73
amanidibo <s374186@oslomet.no>
2024-05-05 03:39:19 +0200

    noor

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('.menu a'); // updated selector
-    window.onscroll = () => {
-        sections.forEach(sec => {
-            let top = window.scrollY;
-            let offset = sec.offsetTop - 150;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id');
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(links => {
-                    links.classList.remove('active');
-                    let activeLink = document.querySelector('.menu a[href*="' + id + '"]');
-                    if (activeLink) {
-                        activeLink.classList.add('active');
-                    }
-                });
-            }
-        });
-    };
-});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll(' .menu a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector(' .menu a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
